9a6469999903a66f0af87418b16d6b58840f231b
Giovani <giovani.alves@vibraconsistemas.com.br>
2023-08-22 14:48:49 -0300

    extrat code complete, work perfectly

---
+++ src/ports/SIM.Ports.Func.ExtractSensorReading/Program.cs
@@ -0,0 +1,30 @@
+﻿using OfficeOpenXml.Packaging.Ionic.Zlib;
+using System;
+using System.IO;
+
+namespace SIM.Ports.Func.ExtractSensorReading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"C:\Users\Coodesh\Documents\Vibracon\sim.func.extract.sensor.reading\src\ports\SIM.Ports.Func.ExtractSensorReading\COMP";
+            string outputFilePath = @"C:\Users\Coodesh\Documents\Vibracon\sim.func.extract.sensor.reading\src\ports\SIM.Ports.Func.ExtractSensorReading\output.txt";
+
+            using (FileStream inputStream = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (MemoryStream outputStream = new MemoryStream())
+                {
+                    using (ZlibStream deflateStream = new ZlibStream(inputStream, CompressionMode.Decompress))
+                    {
+                        deflateStream.CopyTo(outputStream);
+                    }
+                    byte[] decompressedBytes = outputStream.ToArray();
+                    // Salvar os bytes descompactados em um arquivo
+                    File.WriteAllBytes(outputFilePath, decompressedBytes);
+                    Console.WriteLine("Arquivo zlib descompactado com sucesso.");
+                }
+            }
+        }
+    }
+}
